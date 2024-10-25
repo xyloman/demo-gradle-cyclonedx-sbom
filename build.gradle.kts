6259f43166520eba4f866c65d5d40244f3d1b16a
Bryan Kelly <bkelly@gradle.com>
2024-10-25 11:57:01 -0500

    Added package publishing.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,30 +1,54 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.3.0"
-	id("io.spring.dependency-management") version "1.1.6"
-	id("org.cyclonedx.bom") version "1.10.0"
+    java
+    `maven-publish`
+    id("org.springframework.boot") version "3.3.0"
+    id("io.spring.dependency-management") version "1.1.6"
+    id("org.cyclonedx.bom") version "1.10.0"
 }
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.keycloak:keycloak-services:24.0.4")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.keycloak:keycloak-services:24.0.4")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
+}
+
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "${group}"
+            artifactId = "library"
+            version = "${version}"
+
+            from(components["java"])
+
+            repositories {
+                maven {
+                    url = uri("https://maven.pkg.github.com/xyloman/demo-gradle-cylconedx-sbom")
+                    credentials {
+                        username = System.getenv("GITHUB_ACTOR")
+                        password = System.getenv("GITHUB_TOKEN")
+                    }
+                }
+            }
+        }
+    }
 }
